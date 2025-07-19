03de366cea1ce0b1a7ac4cd299f95a1b577ce22e
Ernesto <SKneto.O@gmail.com>
2025-07-19 17:53:41 -0600

    New Line

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -72,6 +72,7 @@ export function LoginForm() {
           <InfoIcon className="h-4 w-4" />
           <AlertDescription>
             <strong>isDemoMode: {isDemoMode() ? 'TRUE': 'FALSE'}</strong><br />
+            <br/ >
             <strong>Demo Credentials:</strong><br />
             Email: admin@hestiaplp.com.mx<br />
             Password: password123
