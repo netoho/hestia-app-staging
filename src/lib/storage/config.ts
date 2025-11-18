dcff8ebb05ab5c4e51444bb77a40dd2310d95673
neto <ernesto.huerta@neximo.mx>
2025-11-18 16:31:17 -0600

    chore: missing config for the s3 storage

--- src/lib/storage/config.ts
+++ src/lib/storage/config.ts
@@ -16,6 +16,7 @@ export function getStorageConfig(): StorageConfig {
     case 's3':
       config.s3 = {
         bucket: process.env.AWS_S3_BUCKET || '',
+        publicBucket: process.env.AWS_PUBLIC_S3_BUCKET || 'public-hestia-files',
         region: process.env.AWS_S3_REGION || 'us-east-1',
         accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
