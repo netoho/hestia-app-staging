cd449ea81a1a642c513360f8bdadf0272c8b4317
neto <ernesto.huerta@neximo.mx>
2025-11-29 21:47:33 -0600

    chore: using the same drive

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,24 +1,12 @@
 
 import { PrismaClient } from '@/prisma/generated/prisma-client/client'
 import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaPostgresAdapter } from '@prisma/adapter-ppg'
 
-const config = {
-  transactionOptions: { timeout: 300000, maxwait: 300000 },
-}
-
-if (process.env.IS_LOCAL === 'local') {
-  // @ts-ignore
-  config['adapter'] = new PrismaPg({
-    connectionString: process.env.DATABASE_URL
-  })
-} else {
-  // @ts-ignore
-  config['adapter'] = new PrismaPostgresAdapter({
+export const prisma = new PrismaClient({
+  transactionOptions: { timeout: 300000, maxWait: 300000 },
+  adapter: new PrismaPg({
     connectionString: process.env.DATABASE_URL!,
   })
-}
-
-export const prisma = new PrismaClient(config)
+})
 
 export default prisma;
